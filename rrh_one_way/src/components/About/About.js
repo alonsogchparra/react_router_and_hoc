38d74f4ca4223261ef38e8ddf982b054698780ee
Alonso Parra <alonsogparra@gmail.com>
2019-10-03 11:14:14 -0400

    About component updated with hoc on it

--- rrh_one_way/src/components/About/About.js
+++ rrh_one_way/src/components/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Rainbow from '../../hoc/Rainbow';
 
 const about = (props) => (
-  <div>
-    About Page
+  <div className="container">
+    <div className="center">
+      <h3>About Page</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione rerum odio magnam pariatur ad temporibus, vero odit earum? Deserunt fuga animi distinctio cum non reiciendis et excepturi eos labore a!</p>
+    </div>
   </div>
 );
 
-export default about;
+export default Rainbow(about);
