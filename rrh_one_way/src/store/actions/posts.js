6f1cb554d478eac73de30da0e0948c42a27b456b
Alonso Parra <alonsogparra@gmail.com>
2019-10-04 09:22:22 -0400

    post action updated with getPosts to get the posts from jsonplaceholder, and action creator added loadingPosts

---
+++ rrh_one_way/src/store/actions/posts.js
@@ -0,0 +1,18 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const getPosts = () => {
+  return dispatch => {
+    return axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        dispatch (loadingPosts(res.data));
+      })
+  }
+}
+
+export const loadingPosts = (posts) => {
+  return {
+    type: actionTypes.LOADING_POSTS,
+    posts
+  }
+}
