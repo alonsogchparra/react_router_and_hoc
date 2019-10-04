6255ffda8cded5a54d7b501f7e19421685895889
Alonso Parra <alonsogparra@gmail.com>
2019-10-04 09:43:02 -0400

    Redux added on Home component to show posts

--- rrh_one_way/src/components/Home/Home.js
+++ rrh_one_way/src/components/Home/Home.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 
 class Home extends Component {
+
+  componentDidMount() {
+    if (this.props.posts.length) {
+      return;
+    } else {
+      this.props.ongetPosts();
+    }
+  }
+
   render () {
+
+    console.log('home', this.props);
     return (
       <div>
-        Home Page
+        {this.props.posts.map(post => <li key={post.id}>{post.title} ID:{post.id}</li>)}
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = state => {
+  return {
+    posts: state.posts.posts
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    ongetPosts: () => dispatch(actions.getPosts())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
