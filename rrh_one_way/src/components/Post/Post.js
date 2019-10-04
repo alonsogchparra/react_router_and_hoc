addbfde8d32c46f86b0b2e918cd1381214d0e471
Alonso Parra <alonsogparra@gmail.com>
2019-10-04 12:59:17 -0400

    Adding redux  on Post and using the delete action

--- rrh_one_way/src/components/Post/Post.js
+++ rrh_one_way/src/components/Post/Post.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 
 class Post extends Component {
+
+  deletePostHandler = () => {
+    this.props.onDeletePost(this.props.location.state.id);
+    this.props.history.push('/');
+  }
+
   render () {
     return (
       <div className="container center">
         <h4 className="red-text">{this.props.location.state.title}</h4>
         <p>{this.props.location.state.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.deletePostHandler }>DELETE POST</button>
+        </div>
       </div>
     )
   }
 }
 
-export default Post
+const mapDispatchToProps = dispatch => {
+  return {
+    onDeletePost: (id) => dispatch(actions.deletePost(id))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Post)
