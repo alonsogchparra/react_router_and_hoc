8e034daeec9487df1617ee02f55122f51add66eb
Alonso Parra <alonsogparra@gmail.com>
2019-10-07 23:05:30 -0400

    About built with Rainbow HOC

--- rrh_two_way/src/components/About/About.js
+++ rrh_two_way/src/components/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Rainbow from '../../hoc/Rainbow';
 
 const about = (props) => (
-  <div>
-    About Page
+  <div className="container">
+    <div className="center">
+      <h3>About Page</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas sint impedit corporis officiis, optio laborum provident ipsum at ullam molestias. Dicta aliquid voluptatum ex necessitatibus in, expedita itaque odio impedit corrupti, officia minima adipisci provident!</p>
+    </div>
   </div>
 );
 
-export default about;
+export default Rainbow(about);
