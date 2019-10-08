3ec5a1229f15cc967d0fbfb002df80e14329d00a
Alonso Parra <alonsogparra@gmail.com>
2019-10-07 23:08:30 -0400

    Contact component built

--- rrh_two_way/src/components/Contact/Contact.js
+++ rrh_two_way/src/components/Contact/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const contact = (props) => (
-  <div>
-    Contact Page
+  <div className="container">
+    <div className="center">
+      <h3>Contact Page</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dicta saepe praesentium id! Pariatur incidunt nobis quisquam consequatur nostrum eveniet deserunt, dolorum deleniti! Quam reiciendis velit fuga reprehenderit a rerum sit inventore nemo quas repudiandae.</p>
+    </div>
   </div>
 );
 
