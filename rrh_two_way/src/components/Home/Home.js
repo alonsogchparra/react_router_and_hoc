f14ace949af36bd0825d02889d9fe77f18308168
Alonso Parra <alonsogparra@gmail.com>
2019-10-07 23:26:49 -0400

    Home built with axios to get the postsList from jsonplaceholder

--- rrh_two_way/src/components/Home/Home.js
+++ rrh_two_way/src/components/Home/Home.js
@@ -1,10 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Spinner from '../Spinner/Spinner';
+import axios from 'axios';
 
 class Home extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
+
   render () {
+    const postsList = this.state.posts.length ? (
+      this.state.posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title red-text">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="container center">
+        <div>Not posts yet.</div>
+        <Spinner />
+      </div>
+    );
     return (
-      <div>
-        Home Page
+      <div className="container home">
+        <h4 className="center">Home</h4>
+        {postsList}
       </div>
     )
   }
