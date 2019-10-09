26a8b832b28090bfaebf18f04167a4a759571c11
Alonso Parra <alonsogparra@gmail.com>
2019-10-09 16:56:53 -0400

    index updated with reducer

--- rrh_two_way/src/index.js
+++ rrh_two_way/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, compose } from 'redux';
 import './index.css';
 import App from './App';
+import postsReducer from './store/reducers/posts';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  posts: postsReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers());
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
