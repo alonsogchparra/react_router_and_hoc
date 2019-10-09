bbe79afea86b9f4d9bc477e84eb894fc593c86eb
Alonso Parra <alonsogparra@gmail.com>
2019-10-09 16:57:48 -0400

    Home component updated with redux

--- rrh_two_way/src/components/Home/Home.js
+++ rrh_two_way/src/components/Home/Home.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import Spinner from '../Spinner/Spinner';
-import axios from 'axios';
+import { connect } from 'react-redux';
+// import axios from 'axios';
 
 class Home extends Component {
 
-  state = {
-    posts: []
-  }
+  // state = {
+  //   posts: []
+  // }
 
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        this.setState({
-          posts: res.data.slice(0, 10)
-        })
-      })
-  }
+  // componentDidMount() {
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(res => {
+  //       this.setState({
+  //         posts: res.data.slice(0, 10)
+  //       })
+  //     })
+  // }
 
   render () {
-    const postsList = this.state.posts.length ? (
-      this.state.posts.map(post => {
+    const postsList = this.props.posts.length ? (
+      this.props.posts.map(post => {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
@@ -44,4 +45,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = state => {
+  return {
+    posts: state.posts.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home);
